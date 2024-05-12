185cdeffe8e59fa14b3a1e3a1a92e16b375aaafd
KFC-Tim <42516650+KFC-Tim@users.noreply.github.com>
2024-05-12 19:47:55 +0200

    added Field Scripts

---
+++ Assets/Scripts/Field/EventField.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventField : Field
+{
+    public override bool IsEventField()
+    {
+        return false;
+    }
+}
