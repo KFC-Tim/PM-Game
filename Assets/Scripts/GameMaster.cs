d103eb9fec8657f5a56fe7c81e83c9177ddcd63c
Ta1to <simon.hertweck.sh@gmail.com>
2024-05-22 19:39:39 +0200

    #01: Question handlinng + player pieces

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -1,24 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class GameMaster : MonoBehaviour
 {
     public QuestionsController QuestionsController;
+    public TMP_InputField inputField;
+    public Button submitButton;
 
+    public PlayerPiece[] playerPiecePrefab;
+
+    private PlayerPiece[,] playerPieces;
+    private Questions currentQuestion;
     private int currentPlayerIndex = 0;
     private int totalPlayers = 4; // required to have 4 players
+    private int piecesForPlayer = 4;
     private bool gameIsOver = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        InitializePlayerPieces();
+        submitButton.onClick.AddListener(SubmitAnswer);
+
         // maybe here the random or by join the lobby
         currentPlayerIndex = 0;
         gameIsOver = false;
+
+        // Starts the game
+        AtTurn();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -27,18 +44,16 @@ public class GameMaster : MonoBehaviour
     }
 
 
-    // Called if a player is at the turn
+    // Called if a player is at the turn    
     public void AtTurn()
     {
         // Get a question
-        Questions question = QuestionsController.AskQuestion();
+        currentQuestion = QuestionsController.AskQuestion();
         Debug.Log("Player " + (currentPlayerIndex + 1) + " answer this qustions:");
-        Debug.Log(question);
+        Debug.Log(currentQuestion.questionText);
 
-        // Here comes the UserInput
-        string playerAnswer = "";
-
-        QuestionsController.EvaluateAnswer(playerAnswer, question); 
+        inputField.gameObject.SetActive(true);
+        submitButton.gameObject.SetActive(true);
 
     }
 
@@ -64,8 +79,59 @@ public class GameMaster : MonoBehaviour
         // Show in an alert or somewhere
         Debug.Log("Player " + (currentPlayerIndex + 1) +"'s turn.");
 
+        AtTurn();
+
     }
 
+    // Player can select one player piece to move forword with
+    private void SelectPlayerToMove()
+    {
+
+    }
+
+    // Player moves forword with his piece
+    private void MovePlayerPiece(int playerIndex, int steps)
+    {
+
+    }
+
+    // Initialize all the player pieces and give them a position
+    private void InitializePlayerPieces()
+    {
+        playerPieces = new PlayerPiece[totalPlayers, piecesForPlayer];
+
+        for(int i = 0; i < totalPlayers; i++)
+        {
+            for(int j = 0;  j < piecesForPlayer; j++)
+            {
+                PlayerPiece newPiece = Instantiate(playerPiecePrefab[i], CalculateStartPosition(i, j), Quaternion.identity);
+                newPiece.name = "Player " + (i + 1) + " Piece " + (j + 1);
+                newPiece.transform.parent = transform;
+                playerPieces[i, j] = newPiece;
+            }
+        }
+    }
+
+    // Submit the answer an evalutes the answer
+    private void SubmitAnswer()
+    {
+        string playerAnswer = inputField.text;
+        QuestionsController.EvaluateAnswer(playerAnswer, currentQuestion);
+
+        inputField.gameObject.SetActive(false);
+        submitButton.gameObject.SetActive(false);
+
+        inputField.text = "";
+
+        EndTurn();
+    }
+
+    // Calcualtes the start position of the player pieces
+    private Vector3 CalculateStartPosition(int playerIndex, int pieceIndex)
+    {
+        // Here comes the calcualtion
+        return new Vector3(playerIndex, 0, pieceIndex);
+    }
 
     private bool CheckForWin()
     {
--- Assets/Scripts/Questions/Questions.cs
+++ Assets/Scripts/Questions/Questions.cs
@@ -19,5 +19,4 @@ public class Questions
         this.steps = steps;
     }
 
-
 }
