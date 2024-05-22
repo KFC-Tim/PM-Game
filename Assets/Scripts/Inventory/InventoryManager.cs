032e99b83d6beaaff722e3be2a2ac58ccbadd357
KFC-Tim <42516650+KFC-Tim@users.noreply.github.com>
2024-05-22 22:10:54 +0200

    added Inventory

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject textPrefab;
+
+    private List<FieldEvent> events = new List<FieldEvent>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void AddCard(FieldEvent eventC) {
+        events.Add(eventC);
+        UpdateInventoryUI();
+    }
+
+    public void RemoveCard(FieldEvent eventC) {
+        events.Remove(eventC);
+        UpdateInventoryUI();
+    }
+
+    void UpdateInventoryUI()
+    {
+        foreach (Transform child in panel.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (eventC in events)
+        {
+            GameObject newText = Instantiate(textPrefab, panel.transform);
+            newText.GetComponent<Text>().text = eventC.name;
+        }
+    }
+}
