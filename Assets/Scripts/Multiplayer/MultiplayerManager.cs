9191af11413623af83e052f4e08b948f521898ec
Ole Schmid <yatibluetv@gmail.com>
2024-05-29 17:19:06 +0200

    update multiplayer

--- Assets/Scripts/Multiplayer/MultiplayerManager.cs
+++ Assets/Scripts/Multiplayer/MultiplayerManager.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
-using WebSocketSharp;
+using Unity.Networking.Transport;
+using Unity.Collections;
+using System.Text;
 using System.Collections.Generic;
-using System.Net.WebSockets;
-using UnityEngine.Networking.PlayerConnection;
+using System;
 
-
-public class WebSocketClient : MonoBehaviour
+public class MultiplayerManager : MonoBehaviour
 {
-    private WebSocket ws;
+    private NetworkDriver driver;
+    private NetworkConnection connection;
     public string playerName;
     public int playerIndex;
     public GameState gameState;
@@ -15,12 +16,52 @@ public class WebSocketClient : MonoBehaviour
 
     void Start()
     {
-        ws = new WebSocket("ws://87.106.165.86:8080");
-        ws.OnMessage += OnMessageReceived;
-        ws.Connect();
-        playerName = "Player" + Random.Range(1, 100);
+        driver = NetworkDriver.Create();
+        connection = default(NetworkConnection);
+        var endpoint = NetworkEndpoint.Parse("87.106.165.86", 8080);
+        connection = driver.Connect(endpoint);
     }
 
+    void Update()
+    {
+        driver.ScheduleUpdate().Complete();
+
+        if (!connection.IsCreated)
+        {
+            return;
+        }
+
+        DataStreamReader stream;
+        NetworkEvent.Type cmd;
+
+        while ((cmd = connection.PopEvent(driver, out stream)) != NetworkEvent.Type.Empty)
+        {
+            if (cmd == NetworkEvent.Type.Connect)
+            {
+                Debug.Log("We are now connected to the server");
+            }
+            else if (cmd == NetworkEvent.Type.Data)
+            {
+                // Read incoming data length and bytes
+                int length = stream.Length;
+                var buffer = new NativeArray<byte>(length, Allocator.Temp);
+                stream.ReadBytes(buffer);
+
+
+                // Convert byte array to string and
+
+                var message = Encoding.UTF8.GetString(buffer.ToArray());
+                OnMessageReceived(message);
+            }
+            else if (cmd == NetworkEvent.Type.Disconnect)
+            {
+                Debug.Log("Client got disconnected from server");
+                connection = default(NetworkConnection);
+            }
+        }
+    }
+
+    // Method to create a game
     public void CreateGame()
     {
         var createMessage = new
@@ -28,9 +69,10 @@ public class WebSocketClient : MonoBehaviour
             type = "create",
             playerName = playerName
         };
-        ws.Send(JsonUtility.ToJson(createMessage));
+        SendMessage(JsonUtility.ToJson(createMessage));
     }
 
+    // Method to join an existing game
     public void JoinGame(string gameId)
     {
         this.gameId = gameId;
@@ -40,12 +82,13 @@ public class WebSocketClient : MonoBehaviour
             playerName = playerName,
             gameId = gameId
         };
-        ws.Send(JsonUtility.ToJson(joinMessage));
+        SendMessage(JsonUtility.ToJson(joinMessage));
     }
 
-    void OnMessageReceived(object sender, MessageEventArgs e)
+    // Handle received messages
+    void OnMessageReceived(string message)
     {
-        var data = JsonUtility.FromJson<ServerMessage>(e.Data);
+        var data = JsonUtility.FromJson<ServerMessage>(message);
         switch (data.type)
         {
             case "gameCreated":
@@ -60,30 +103,62 @@ public class WebSocketClient : MonoBehaviour
                 gameState = data.state;
                 UpdateUI();
                 break;
+            case "question":
+                DisplayQuestion(data.questionData);
+                break;
             case "error":
                 Debug.LogError("Error: " + data.message);
                 break;
             default:
+                Debug.LogWarning("Unhandled message type: " + data.type);
                 break;
         }
     }
 
-    public void SendAnswer(int questionId, string answer)
+
+    public class QuestionData
     {
-        var answerMessage = new
+        public string question;
+        public string[] answers;
+    }
+
+    private void DisplayQuestion(QuestionData questionData)
+    {
+        throw new NotImplementedException();
+    }
+
+    new
+        void SendMessage(string message)
+    {
+        DataStreamWriter writer;
+        if (driver.BeginSend(connection, out writer) == 0)
         {
-            type = "answer",
-            gameId = gameId,
-            playerIndex = playerIndex,
-            questionId = questionId,
-            answer = answer
-        };
-        ws.Send(JsonUtility.ToJson(answerMessage));
+            var bytes = Encoding.UTF8.GetBytes(message);
+            writer.WriteInt(bytes.Length);
+            using (var nativeArray = new NativeArray<byte>(bytes, Allocator.Temp))
+            {
+                writer.WriteBytes(nativeArray);
+            }
+            var status = driver.EndSend(writer);
+            if (status < 0)
+            {
+                Debug.LogError("Failed to send message: " + status);
+            }
+        }
+        else
+        {
+            Debug.LogError("Failed to begin send.");
+        }
     }
 
     void UpdateUI()
     {
         // Update your UI based on the new game state
+    }
+
+    void OnDestroy()
+    {
+        driver.Dispose();
     }
 
     [System.Serializable]
@@ -93,14 +168,23 @@ public class WebSocketClient : MonoBehaviour
         public string gameId;
         public GameState state;
         public string message;
+        public QuestionData questionData;
     }
 
     [System.Serializable]
     public class GameState
     {
-        public List<string> players;
+        public List<PlayerInfo> players;
         public int currentTurn;
-        public List<string> questions;
+
+        // Dictionary mapping player UUIDs to their scores
         public Dictionary<string, int> scores;
+    }
+
+    [System.Serializable]
+    public class PlayerInfo
+    {
+        public string uuid;
+        public string name;
     }
 }
