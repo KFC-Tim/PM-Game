340663935799fee00d12ffd1bbd405b5a676411a
Ta1to <simon.hertweck.sh@gmail.com>
2024-06-13 10:48:43 +0200

    Updated Gameboard

--- Assets/Scripts/Multiplayer/ClientGameState.cs
+++ Assets/Scripts/Multiplayer/ClientGameState.cs
@@ -11,6 +11,8 @@ public class ClientGameState : MonoBehaviour
     private GameState gameState = null;
     private string gameId = "";
 
+    
+
     // Start is called before the first frame update
     void Start()
     {
