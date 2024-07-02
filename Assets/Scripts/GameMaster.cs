653aa1512f5b1149257810083624bc7e6dbb6ad1
KFC-Tim <42516650+KFC-Tim@users.noreply.github.com>
2024-07-02 14:43:46 +0200

    only active players

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -46,7 +46,9 @@ public class GameMaster : MonoBehaviour, IGameController
         Debug.Log("GameMaster started!!");
         CrossSceneInformation.SetGameMasterLoaded(true);
         CrossSceneInformation.SetGameMasterInstance(this);
-        InitializePlayerPieces();
+        //InitializePlayerPieces();
+        playerPieces = new List<PlayerPiece>();
+        
         questionsController.SetGameController(this);
         
         if(board == null){
@@ -110,15 +112,12 @@ public class GameMaster : MonoBehaviour, IGameController
         }
         
         Debug.Log(playerPieces.Count);
-        if (playerPieces.Count <= 0)
-        {
-            return;
-        }
+        
         foreach (var player in gameState.players)
         {
             try
             {
-                if (i > playerPieces.Count)
+                if (i >= playerPieces.Count)
                 {
                     Debug.Log("Creating new Player Piece");
                     Vector3 offset = new Vector3(0, 0.35f, 0);
@@ -131,8 +130,8 @@ public class GameMaster : MonoBehaviour, IGameController
                     {
                         meshRenderer.enabled = true;
                     }
-
-                    playerPieces[i] = pieceInstance.GetComponent<PlayerPiece>();
+                    
+                    playerPieces.Add(pieceInstance.GetComponent<PlayerPiece>());
                     playerPieces[i].SetPath(GetBoardPathForTeam(i));
                 }
 
